96d8e0776ceff3f59d2f941dcb934dbd526651f9
Sanny Kumar <sannykumar085@gmail.com>
2021-05-06 16:15:10 +0530

    Repository added

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import {
+    Card,
+    CardActionArea,
+    Grid,
+    Grow,
+    Tabs,
+    Typography
+} from "@material-ui/core";
+import Tab from '@material-ui/core/Tab';
+import {Button} from 'reactstrap';
+import './NavHeader.css'
 
 const NavHeader = () => {
-    return (
-        <div>
-            <h1>This is navHeader</h1>
-        </div>
+    const [tabvalue, setTabvalue] = useState("Overview");
+   
+    const ResumeData = {
+        projects : [
+                   {tag:'Repository'},
+                   {tag:'Projects'},
+                   {tag:'Projects'},
+                   {tag:'Packages'}
+                ]
+            };
+return (
+<>
+{/*Tabs*/}
+<Grid item xs={12}>
+    <Tabs value={tabvalue} indicatorColor="white" className="customTabs" 
+        onChange={(event, newValue) =>{setTabvalue(newValue) }}>
+
+        <Tab label="Overview" value="Overview" 
+            className={ tabvalue === 'Overview' ? 'customTabs_item active mx-2' : 'customTabs_item mx-2'} />   
+        {/*Set method remove Duplicate value from listing*/}
+        { [...new Set(ResumeData.projects.map((items) => items.tag ))].map( (n)=>  (
+            <Tab label={n}  value={n} className={tabvalue === n ? 'customTabs_item active mx-2' : 'customTabs_item mx-2'}/>
+          ) )
+        }
+    </Tabs>
+</Grid>
+{/*End Tabs*/}
+
+{/*Project*/}
+<Grid container>
+    <Grid item xs={12}>
+     {
+        tabvalue === "Repository" ? (
+            <>
+            <div class="d-flex flex-row-reverse bd-highlight">
+              <Button color="success ">Add New </Button> 
+            </div>
+            </>
+        ) : null 
+     }
+    </Grid>
+ </Grid>
+{/*End Project*/}
+ 
+
+</>
     )
 }
 
--- src/components/UserCart.jsx
+++ src/components/UserCart.jsx
@@ -5,20 +5,27 @@ import { GoLocation } from "react-icons/go";
 
 const UserCart = ({user}) => {
     return (
+        <>
+        {user?.login ?  
+        (<>
         <Card className="mb-4"> 
-            <img src={user?.avatar_url} alt={user?.login} className="p-1" style={{borderRadius:'50%',width:'275px'}}/>
-            <CardBody>
-              <h2 className="card-title text-left">{user?.name}</h2>
-              <span className="card-subtitle text-left text-secondary">{user?.login}</span><br/> 
-              
-              <p className="text-dark p-0"><span style={{fontSize:'14px'}}>{user?.bio}</span></p>
-              <div className="text-dark">{user?.followers ?  (user.followers + " followers") : " "}&nbsp;&nbsp;&nbsp;&nbsp;{user?.following ?  (user.following + " following") : " "}</div>
-              <br/>
-              <span className="text-muted">{(user?.location) ?   ( "Location : "+user?.location) : (" ") }</span>
-              <div className="text-danger">{ (user?.hireable) ? ("Available for Hier : "+(user?.hireable)) ? ("Yes") : ("Noo") : " "} </div>
-            
-            </CardBody>
-        </Card>
+        <img src={user?.avatar_url} alt={user?.login} className="p-1" style={{borderRadius:'50%',width:'275px'}}/>
+        <CardBody>
+          <h2 className="card-title text-left">{user?.name}</h2>
+          <span className="card-subtitle text-left text-secondary">{user?.login}</span><br/> 
+          
+          <p className="text-dark p-0"><span style={{fontSize:'14px'}}>{user?.bio}</span></p>
+          <div className="text-dark">{user?.followers ?  (user.followers + " followers") : " "}&nbsp;&nbsp;&nbsp;&nbsp;{user?.following ?  (user.following + " following") : " "}</div>
+          <br/>
+          <span className="text-muted">{(user?.location) ?   ( "Location : "+user?.location) : (" ") }</span>
+          <div className="text-danger">{ (user?.hireable) ? ("Available for Hier : "+(user?.hireable)) ? ("Yes") : ("Noo") : " "} </div> 
+        </CardBody>
+       </Card>
+       </>)
+        : null
+       }
+       </>
+       
     )
 }
  
