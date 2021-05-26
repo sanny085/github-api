6603cdcac9a53bf2e25ff62665cfd66e58ddccf7
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-05-26 14:33:24 +0530

    Update reducer.js

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -10,14 +10,11 @@ import {
 export default (state, action) => {
  switch (action.type) {
    case SET_CONTACT:
-    return action.payload === null ? {...state, contacts:[]} : {...state, contacts: action.payload};
-    
+    return action.payload === null ? {...state, contacts:[]} : {...state, contacts: action.payload};  
    case SET_LOADING:
     return {...state, isLoading: action.payload};
-
    case CONTACT_TO_UPDATE:
     return {...state, contactToUpdate: action.payload, contactToUpdateKey: action.key};
-   
    case SET_SINGLE_CONTACT:
     return {...state, contact: action.payload};  
    default:
